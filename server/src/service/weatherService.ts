a0a4eea73c6cefb65c9ddcef0a2b1003625c5aa0
Keming <hu000157@umn.edu>
2025-02-27 18:23:18 -0700

    weatherServices completed

--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -1,31 +1,99 @@
 import dotenv from 'dotenv';
+import axios from 'axios';
 dotenv.config();
 
 // TODO: Define an interface for the Coordinates object
-
+interface Coordinates {
+  lat: number;
+  lon: number;
+}
 // TODO: Define a class for the Weather object
+class Weather {
+  constructor(
+    public temperature: number,
+    public description: string,
+    public icon: string
+  ) {}
+}
 
 // TODO: Complete the WeatherService class
 class WeatherService {
   // TODO: Define the baseURL, API key, and city name properties
+  private baseURL: string;
+  private apiKey: string;
+
+  constructor() {
+    this.baseURL = 'https://api.openweathermap.org/data/2.5';
+    this.apiKey = process.env.OPENWEATHERMAP_API_KEY || '';
+  }
   // TODO: Create fetchLocationData method
   // private async fetchLocationData(query: string) {}
+  private async fetchLocationData(query: string): Promise<any> {
+    const response = await axios.get(`${this.baseURL}/weather`, {
+      params: {
+        q: query,
+        appid: this.apiKey,
+      },
+    });
+    return response.data;
+  }
   // TODO: Create destructureLocationData method
   // private destructureLocationData(locationData: Coordinates): Coordinates {}
-  // TODO: Create buildGeocodeQuery method
-  // private buildGeocodeQuery(): string {}
+  private destructureLocationData(locationData: any): Coordinates {
+    return {
+      lat: locationData.coord.lat,
+      lon: locationData.coord.lon,
+    };
+  }
+
   // TODO: Create buildWeatherQuery method
   // private buildWeatherQuery(coordinates: Coordinates): string {}
+  private buildWeatherQuery(coordinates: Coordinates): string {
+    return `${this.baseURL}/forecast?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${this.apiKey}`;
+  }
   // TODO: Create fetchAndDestructureLocationData method
   // private async fetchAndDestructureLocationData() {}
+  private async fetchAndDestructureLocationData(city: string): Promise<Coordinates> {
+    const locationData = await this.fetchLocationData(city);
+    return this.destructureLocationData(locationData);
+  }
   // TODO: Create fetchWeatherData method
   // private async fetchWeatherData(coordinates: Coordinates) {}
+  private async fetchWeatherData(coordinates: Coordinates): Promise<any> {
+    const response = await axios.get(this.buildWeatherQuery(coordinates));
+    return response.data;
+  }
+
   // TODO: Build parseCurrentWeather method
   // private parseCurrentWeather(response: any) {}
+  private parseCurrentWeather(response: any): Weather {
+    const currentWeather = response.list[0];
+    return new Weather(
+      currentWeather.main.temp,
+      currentWeather.weather[0].description,
+      currentWeather.weather[0].icon
+    );
+  }
   // TODO: Complete buildForecastArray method
   // private buildForecastArray(currentWeather: Weather, weatherData: any[]) {}
+  private buildForecastArray(weatherData: any[]): Weather[] {
+    return weatherData.map((data: any) => {
+      return new Weather(
+        data.main.temp,
+        data.weather[0].description,
+        data.weather[0].icon
+      );
+    });
+  }
   // TODO: Complete getWeatherForCity method
   // async getWeatherForCity(city: string) {}
+  public async getWeatherForCity(city: string): Promise<{ current: Weather; forecast: Weather[] }> {
+    const coordinates = await this.fetchAndDestructureLocationData(city);
+    const weatherData = await this.fetchWeatherData(coordinates);
+    const currentWeather = this.parseCurrentWeather(weatherData);
+    const forecast = this.buildForecastArray(weatherData.list);
+    return { current: currentWeather, forecast };
+  }
 }
 
 export default new WeatherService();
